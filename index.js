06b77716d6ce21419fc58c9b1a940c5d3c2a8516
Daniel <danielpp95@gmail.com>
2022-05-24 02:13:43 +0100

    feat: ignore output for functions and variable declarations

--- index.js
+++ index.js
@@ -13,12 +13,14 @@ $code.addEventListener('input', (e) => {
     let output = ''
     for(let i = 0; i < numberOfLines; i++) {
         let result = '';
-        const statement = splittedCode.filter(x => x.rowNumber == i + 1)[0].statement || ''
+        const statement = splittedCode.filter(x => x.rowNumber == i + 1)[0]?.statement || ''
 
         if (statement) {
-            let tempCode = splittedCode.filter(x => x.rowNumber < i + 2).map(x => x.statement).join('\n')
-            console.log(tempCode)
-            result = eval(tempCode) || ''
+            const statementsArray = splittedCode.slice(0, i + 1).map(x => x.statement)
+            // console.log(statementsArray, i)
+            let tempCode = statementsArray.join(';')
+            // console.log(tempCode, i)
+            result = isIgnoredOutput(statement) ? '' : eval(tempCode) || ''
         }
 
         output += `${result}\n`
@@ -29,11 +31,42 @@ $code.addEventListener('input', (e) => {
 })
 
 const splitCode = (code) => {
-    const tempCodeList = []
+    const statementsList = []
+    const codeLines = code.split('\n');
 
-    code.split('\n').forEach((statement, index) => {
-        tempCodeList.push({statement, rowNumber: index + 1})
+    // let statementList = ['']
+
+    tempCode = ''
+    codeLines.forEach((codeLine, index) => {
+        try {
+            const statement = `${codeLines.slice(0, index).join('')}${codeLine};`;
+            // console.log(statement)
+            eval(statement)
+
+            statementsList.push({statement: `${tempCode}${codeLine}`, rowNumber: index + 1})
+            tempCode = ''
+        } catch (error) {
+            tempCode = `${tempCode}${codeLine}`;
+        }
     })
 
-    return tempCodeList;
+    return statementsList;
+}
+
+function isIgnoredOutput (statement) {
+    statement = statement.trim();
+
+    if (statement == '') return true
+
+    const reservedWords = ['function', 'const', 'var', '\n']
+
+    for (let index = 0; index < reservedWords.length; index++) {
+        const word = reservedWords[index];
+        if (statement.startsWith(word)) {
+            return true
+        }
+        
+    }
+
+    return false;
 }
